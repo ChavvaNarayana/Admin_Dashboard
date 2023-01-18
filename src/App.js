c34d4118d15ae62eda5a2beb6db436bc73224051
Chavva <narayanachavva@gail.com>
2023-01-18 22:50:11 +0530

    Dashboard Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Home/Dashboard";
 
 function App() {
   return (
     <div className="App">
+
+      <Routes>
+        <Route path='/' element={ <Dashboard /> } />
+      </Routes>
 
     </div>
   );
